4cefab16a1139ebc6cd6b750445fe56f7a00ef13
hessproject <hessn1@duq.edu>
2016-09-21 23:45:36 -0400

    changed models, added data

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 from rango.models import Category, Page
 # Register your models here.
 
-admin.site.register(Category)
-admin.site.register(Page)
+class CategoryAdmin(admin.ModelAdmin):
+    fields = ["name", "slug", "views", "likes"]
+    list_display = ["name", "slug", "views", "likes",]
+    prepopulated_fields = {"slug":('name',)}
+
+class PageAdmin(admin.ModelAdmin):
+    fields = ["title", "category", "url"]
+    list_display = ["title", "category", "url",]
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
+
