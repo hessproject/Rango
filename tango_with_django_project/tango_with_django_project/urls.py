02f9d493003f3d32f239ffb4c783bfc6b1a6770b
hessproject <hessn1@duq.edu>
2016-09-29 12:40:32 -0400

    refactoring registration functionality

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -17,6 +17,12 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
 from rango import views
+from registration.backends.simple.views import RegistrationView
+
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self, request, user):
+        return '/rango/'
+
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
@@ -24,4 +30,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/',
         include('registration.backends.simple.urls')),
+    url(r'^accounts/register/$',
+        MyRegistrationView.as_view(),
+        name="registration_register"),
 ]
